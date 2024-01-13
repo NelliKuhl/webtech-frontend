e0734f0094d4dbfec2d19788c181fdb470a83bb3
NelliKuhl <nellikuhl@gmx.de>
2024-01-13 11:14:48 +0100

    Anpassungen für Render

--- src/components/__tests__/DynamicList.spec.ts
+++ src/components/__tests__/DynamicList.spec.ts
@@ -1,10 +1,11 @@
 import { describe, it, expect, vi } from 'vitest'
 
-import { mount, shallowMount, flushPromises } from '@vue/test-utils'
+import { shallowMount, flushPromises} from '@vue/test-utils'
 import DynamicList from '@/components/DynamicList.vue'
 import type { List, Zutat } from '@/types'
 import axios from 'axios'
 import { ref } from 'vue'
+import { before } from 'node:test'
 
 
 
@@ -17,12 +18,11 @@ describe('DynamicList', () => {
     { id: 1, zutat: 'Apfel', menge: 42, einheit: 'kg'},
     { id: 2, zutat: 'Milch', menge: 13, einheit: 'Stück'}]
   const oneZutat: Zutat[] = [
-    { id: 1, zutat: 'Apfel', menge: 42, einheit: 'kg'}
+    { id: 1, zutat: 'Apfel', menge: 42, einheit: 'Gramm'}
   ]
   const emptylist: List[] = []
-  const filledlist: List[] = [
-    { id: 1, name: 'Einkaufsliste', owner: email},
-    { id: 2, name: 'Einkaufsliste', owner: email}
+  const onefilledlist: List[] = [
+    { id: 1, name: 'Einkaufsliste', owner: email}
   ]
 
   vi.mock('axios')
@@ -30,17 +30,66 @@ describe('DynamicList', () => {
     return { useAuth: () => ({ getUser: vi.fn() })}
   });
 
+
+
   /**
    * Testet, ob eine Nachricht angezeigt wird, wenn keine Items vom Backend empfangen werden.
    */
   it('should display message when no items are received from backend', async () => {
     vi.mocked(axios, true).get.mockResolvedValueOnce({ data: emptyZutat })
 
-    const wrapper = mount(DynamicList)
+    const wrapper = shallowMount(DynamicList)
 
     await flushPromises()
 
     expect(wrapper.text()).toContain('Liste Leer')
   })
-  
+
+
+
+  /**
+  *   Test für das Hinzufügen einer Zutat:
+   *   Überprüfe, ob das Hinzufügen einer Zutat die Zutatenliste aktualisiert.
+  */
+
+  /**
+  *   Test für das Löschen einer Zutat:
+   *   Überprüfe, ob das Löschen einer Zutat die Zutatenliste korrekt aktualisiert.
+  */
+
+  /**
+   *  Test für das Hinzufügen einer Liste:
+   *  Überprüfe, ob das Hinzufügen einer Liste die Listenliste (ja, das klingt komisch) aktualisiert.
+   */
+
+  /**
+  *   Test für das Löschen einer Liste:
+   *   Überprüfe, ob das Löschen einer Liste die Listenliste korrekt aktualisiert und auch die dazugehörigen Zutaten löscht.
+  */
+
+
+  /**
+  *   Test für die Anzeige von Zutaten:
+   *   Überprüfe, ob die Zutaten korrekt auf der Oberfläche angezeigt werden, wenn sie vorhanden sind.
+  */
+
+
+  /**
+  *   Test für die Auswahl einer Liste:
+   *   Überprüfe, ob das Ändern der ausgewählten Liste die Zutatenliste aktualisiert.
+  */
+
+  /**
+  *   Test für die Verhinderung des Buchstabens 'e':
+   *   Überprüfe, ob das Eingeben des Buchstabens 'e' in das Menge-Feld verhindert wird.
+  */
+
+  /**
+  *   Test für Verhinderung von zu hoher Eingabe
+  */
+
+  /**
+  *   Test für Verhinderung von - Eingaben
+  */
+
 })
